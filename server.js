b5c883b4dfa0f7f6318d15f71af390ec8236986d
aaoopee <antti.pelkonen@iki.fi>
2016-12-10 16:09:19 +0200

    Hash and Salt password

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post('/users', function(req,res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 
@@ -126,7 +126,7 @@ app.post('/users', function(req,res) {
 });
 
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync(/*{force: true}*/).then(function() {
 	app.listen(PORT, function() {
 		console.log('Server started on port '+PORT+'.');
 	});
