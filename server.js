86b6ea4eef3846a35832d6a270247628ca18a95e
aaoopee <antti.pelkonen@iki.fi>
2016-12-10 22:57:45 +0200

    Continued implementation of login

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -123,6 +124,28 @@ app.post('/users', function(req,res) {
 		res.status(400).json(e);
 
 	});
+});
+
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	if(typeof body.email !== 'string' || typeof body.password !== 'string') {
+		return res.status(400).send();
+	}
+
+	db.user.findOne( {
+		where: {
+			email: body.email
+		}
+	}).then(function (oneuser) {
+		if (!oneuser || !bcrypt.compareSync(body.password, oneuser.get('password_hash'))) {
+			return res.status(401).send();
+		}
+
+		res.json(oneuser.toPublicJSON());
+	}, function(e) {
+		res.status(500).send();
+	})
 });
 
 
