30e447b3203781deeb02dc90e991a0a7363dab34
aaoopee <antti.pelkonen@iki.fi>
2017-03-09 11:58:44 +0200

    Added support for logout.

--- middleware.js
+++ middleware.js
@@ -1,13 +1,30 @@
+var cryptojs = require('crypto-js');
+
+
 module.exports = function(db) {
     return {
         requireAuthentication: function (req, res, next) {
-            var token = req.get('Auth');
+            var token = req.get('Auth') || '';
 
-            db.user.findByToken(token).then(function(user){
+            db.token.findOne({
+                where: {
+                    tokenHash: cryptojs.MD5(token).toString()
+                }
+            }).then(function(tokenInstance) {
+                if(!tokenInstance) {
+                    console.error('\nERROR: No matching token found.');
+                    throw new Error();
+                }
+
+                req.token = tokenInstance;
+                return db.user.findByToken(token);
+            }).then(function(user) {
+                console.log('\nUser: '+user);
                 req.user = user;
                 next();
-            }, function() {
-                res.status(401).send();
+            }).catch(function() {
+                console.error('ERROR: Sending unauthorized.');
+                res.status(401).send()
             });
         }
     }
--- server.js
+++ server.js
@@ -145,18 +145,30 @@ app.post('/users', function(req,res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			console.error('/user/login: Failed to create token');
-			res.status(401).send();
-		}
-	}, function() {
+		return db.token.create({
+			token: token
+		});
+
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function() {
 		res.status(401).send();
+	});
+});
+
+//DELET /users/login
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function () {
+		console.error('ERROR: Could not destroy token.');
+		res.status(500).send();
 	});
 });
 
